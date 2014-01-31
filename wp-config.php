579bc1f0b96294e8d94ff152b3234808cfd3e7dc
David Ernst <github@ernsts.us>
2014-01-31 17:35:38 -0500

    updated wp-config.php from -sample to working settings

--- wp-config.php
+++ wp-config.php
@@ -14,24 +14,28 @@
  * @package WordPress
  */
 
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'bitnami_wordpress');
+
+if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+    include( dirname( __FILE__ ) . '/local-config.php' );
+}
+
 define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
 define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
 
 define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/content');
 define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/content');
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
-
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'bn_wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', '4a7ede006b');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:3306');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -48,14 +52,15 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+/* Substitution already done */
+define('AUTH_KEY', 'ca2c4f1d1ec9b7fd2811eba456cb5c4063ad8f9fc357134c90de2fa14d8454a7');
+define('SECURE_AUTH_KEY', '6f3254f476cdffab36e1e36a630ca68660135a0fac9dbdccbc8841c89d0a629e');
+define('LOGGED_IN_KEY', 'eadfd3e9a5364c71eb391f4242dbdeb3f37cb65b80f09ebeec2c383ba1089023');
+define('NONCE_KEY', 'fb5e2c9a6f1329a75021f7abccd2f6b248fa204987415d9709d859750ccd3432');
+define('AUTH_SALT', 'b3d0b773833b1c5ace6a6a5046e601d5fea26c27bfa44003e274cd0ddddde3a5');
+define('SECURE_AUTH_SALT', '36974a6a919202ad47c45a7346b2941efd6fb3693b9edc61df2d6124cef56f21');
+define('LOGGED_IN_SALT', 'b2d20a1ee54acf9a72e659db5c28d7b38a2fd396c49cac6ff2158e18d3de4368');
+define('NONCE_SALT', 'db7192d3010753b0921a0cc3d54a9f443da5799be180ffd36b25dad92c03c29d');
 
 /**#@-*/
 
@@ -88,9 +93,19 @@ define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
+
+
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+define('FS_METHOD', 'ftpext');
+define('FTP_BASE', '/opt/bitnami/apps/wordpress/htdocs/');
+define('FTP_USER', 'bitnami');
+define('FTP_PASS', 'tsa9PLd1QLel6XE9Z9wIbGaEUTnVC6B8rUCPmMeieNkquTXEW2');
+define('FTP_HOST', '127.0.0.1');
+define('FTP_SSL', false);
+
